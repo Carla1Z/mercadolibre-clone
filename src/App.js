bb09cc4eab69a12c95d26ef2d02a47739ab1f8fa
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-12-27 18:00:49 -0300

    maquetado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <div className="App">
+      <h1>Holaaaaaa!!!!!</h1>
+      <Layout />
     </div>
   );
 }
---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header";
+import Product from "../Product";
+
+function Layout() {
+  return (
+    <div>
+      <Header />
+      <Product />
+    </div>
+  );
+}
+
+export default Layout;
