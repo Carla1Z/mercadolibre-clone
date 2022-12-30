01f96f6a3c6209d10dbb55b36037ede550b96271
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-12-30 15:24:59 -0300

    header user menu

--- src/components/MenuUser/index.jsx
+++ src/components/MenuUser/index.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import styles from "./MenuUser.module.css";
+import { HiOutlineUserCircle } from "react-icons/hi";
+import { IoNotificationsOutline } from "react-icons/io5";
+import { FiShoppingCart } from "react-icons/fi";
 
 function MenuUser() {
   return (
-    <div>MenuUser</div>
-  )
+    <div className={styles.menuuser}>
+      <ul className={styles.list}>
+        <li className={styles.user}>
+          <HiOutlineUserCircle />
+          <p>Carla</p>
+        </li>
+        <li>Mis compras</li>
+        <li>Favoritos</li>
+        <li>
+          <IoNotificationsOutline />
+        </li>
+        <li>
+          <FiShoppingCart />
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default MenuUser
+export default MenuUser;
