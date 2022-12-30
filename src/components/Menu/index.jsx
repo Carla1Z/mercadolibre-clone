b6fdb4eaa2dbd2049c80dc8c743f100e2b2b45f7
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-12-30 15:14:30 -0300

    menu header

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import styles from "./Menu.module.css";
 
 function Menu() {
   return (
-    <div>Menu</div>
-  )
+    <div className={styles.menu}>
+      <ul className={styles.list}>
+        <li>Categorías</li>
+        <li>Ofertas</li>
+        <li>Historial</li>
+        <li>Supermercado</li>
+        <li>Moda</li>
+        <li>Vender</li>
+        <li>Ayuda</li>
+      </ul>
+    </div>
+  );
 }
 
-export default Menu
+export default Menu;
