ab3bab2a7d6cd926dba06c0dff7430a482b2e1a6
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-12-30 15:06:06 -0300

    maquetado user address menu header

--- src/components/UserAddress/index.jsx
+++ src/components/UserAddress/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { FiMapPin } from "react-icons/fi";
+import styles from "./UserAddress.module.css";
 
 function UserAddress() {
   return (
-    <div>UserAddress</div>
-  )
+    <div className={styles.useraddress}>
+      <FiMapPin className={styles.icon} />
+      <span>
+        <p className={styles.user}>Enviar a Carla</p>
+        <p className={styles.address}>Av Siempreviva</p>
+      </span>
+    </div>
+  );
 }
 
-export default UserAddress
+export default UserAddress;
