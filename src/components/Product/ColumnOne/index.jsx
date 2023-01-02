8f75891cea044f12a9c531f0ff982fae893c50a2
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-01-02 17:49:44 -0300

    column product

---
+++ src/components/Product/ColumnOne/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./ColumnOne.module.css"
+
+function ColumnOne() {
+  return (
+    <div className={styles.column}>ColumnOne</div>
+  )
+}
+
+export default ColumnOne
---
+++ src/components/Product/ColumnTwo/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./ColumnTwo.module.css"
+
+function ColumnTwo() {
+  return (
+    <div className={styles.column}>ColumnTwo</div>
+  )
+}
+
+export default ColumnTwo
