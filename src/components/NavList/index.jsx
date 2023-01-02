c7d3fa9c3b21bce1d3284f1370c419f9b58c3322
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-01-02 16:39:16 -0300

    navlist

--- src/components/NavList/index.jsx
+++ src/components/NavList/index.jsx
@@ -2,8 +2,16 @@ import styles from "./NavList.module.css"
 
 function NavList() {
   return (
-    <div>
-        NavList</div>
+    <div className={styles.navlist}>
+      <p className={styles.back}>Volver al listado</p>
+<nav>
+  <ol>
+    <li>Joyas y Relojes</li>
+    <li>Joyas y Bijouterie</li>
+    <li>Anillos</li>
+  </ol>
+</nav>
+    </div>
   )
 }
 
