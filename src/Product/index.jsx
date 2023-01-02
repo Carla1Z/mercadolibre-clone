cfc2a3bcbe1387666afd1fef9b1cd0643fe5edc5
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-01-02 17:52:44 -0300

    justify content product

--- src/Product/index.jsx
+++ src/Product/index.jsx
@@ -1,9 +1,14 @@
-import styles from "./Product.module.css"
+import ColumnOne from "../components/Product/ColumnOne";
+import ColumnTwo from "../components/Product/ColumnTwo";
+import styles from "./Product.module.css";
 
 function Product() {
   return (
-    <div className={styles.product}>Product</div>
-  )
+    <div className={styles.product}>
+      <ColumnOne />
+      <ColumnTwo />
+    </div>
+  );
 }
 
-export default Product
+export default Product;
