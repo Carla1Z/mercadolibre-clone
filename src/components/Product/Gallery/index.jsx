a3e3d0d6323cdd35b4755c14284a99b79b8e8f69
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-01-02 21:36:22 -0300

    maquetado galeria

--- src/components/Product/Gallery/index.jsx
+++ src/components/Product/Gallery/index.jsx
@@ -1,9 +1,21 @@
-import styles from "./Gallery.module.css"
+import styles from "./Gallery.module.css";
 
 function Gallery() {
   return (
-    <div className={styles.gallery}>Gallery</div>
-  )
+    <div className={styles.gallery}>
+      <div className={styles.container}>
+        <div className={styles.small}></div>
+        <div className={styles.small}></div>
+        <div className={styles.small}></div>
+        <div className={styles.small}></div>
+        <div className={styles.small}></div>
+        <div className={styles.small}></div>
+        <div className={styles.small}></div>
+        <div className={styles.small}></div>
+      </div>
+      <div className={styles.image}></div>
+    </div>
+  );
 }
 
-export default Gallery
+export default Gallery;
