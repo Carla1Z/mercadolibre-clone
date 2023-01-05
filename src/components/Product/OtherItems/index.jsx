2afc3e883cb85b73d9e334a604be2ecf4c86f4f1
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-01-05 16:44:13 -0300

    card y titulo de la sección otheritems

--- src/components/Product/OtherItems/index.jsx
+++ src/components/Product/OtherItems/index.jsx
@@ -1,9 +1,22 @@
-import styles from "./OtherItems.module.css"
+import styles from "./OtherItems.module.css";
 
 function OtherItems() {
   return (
-    <div className={styles.otheritems}>OtherItems</div>
-  )
+    <div className={styles.otheritems}>
+      <h2 className={styles.title}>Publicaciones del vendedor</h2>
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <img src="" alt="card" />
+        </div>
+        <div className={styles.card}>
+          <img src="" alt="card" />
+        </div>
+        <div className={styles.card}>
+          <img src="" alt="card" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default OtherItems
+export default OtherItems;
