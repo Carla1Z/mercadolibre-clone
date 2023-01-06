5fdd2709c499d7c065d5374b11f1209dca477673
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-01-06 15:38:41 -0300

    descripcion

--- src/components/Product/Description/index.jsx
+++ src/components/Product/Description/index.jsx
@@ -2,7 +2,14 @@ import styles from "./Description.module.css"
 
 function Description() {
   return (
-    <div className={styles.description}>Description</div>
+    <div className={styles.description}>
+      <h2 className={styles.title}>Descripción</h2>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur accusantium enim iure perspiciatis perferendis delectus officia eligendi? Voluptatum ut ab veritatis aspernatur magni magnam, quasi excepturi ex rerum, sapiente consequuntur.
+      Quidem odit delectus exercitationem soluta hic possimus cumque neque fugit in sequi similique earum ipsam voluptatem deserunt adipisci, vero obcaecati quae officia vel eveniet? Quae delectus esse dolorum ea vitae.
+      Minus debitis ad repudiandae eum. Asperiores veritatis, fugiat neque atque, aliquid sint doloremque deserunt porro delectus totam corrupti repellat fuga iure minima adipisci accusantium placeat, eveniet sapiente? Aliquam, quo voluptatum.
+      Facilis expedita nam dignissimos enim unde amet officia, tempora ab iusto nesciunt dolor, ratione non eum odit aspernatur est totam. Quod delectus vitae amet non consequuntur perferendis molestiae provident doloremque?
+      Corrupti dolores repellat architecto odit labore quam, eaque soluta esse. Molestiae, sit? Assumenda unde cumque recusandae nemo hic provident, voluptatum asperiores quidem? Consectetur, ea quia voluptatum optio quae quidem vel.</p>
+      </div>
   )
 }
 
