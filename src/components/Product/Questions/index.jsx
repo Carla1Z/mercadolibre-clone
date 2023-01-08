1f4f435989da7ed122c30cac84a8777f84064680
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-01-08 16:26:36 -0300

    maquetado questions

--- src/components/Product/Questions/index.jsx
+++ src/components/Product/Questions/index.jsx
@@ -1,9 +1,46 @@
-import styles from "./Questions.module.css"
+import styles from "./Questions.module.css";
 
 function Questions() {
   return (
-    <div className={styles.questions}>Questions</div>
-  )
+    <div className={styles.questions}>
+      <h2 className={styles.title}>Preguntas y respuestas</h2>
+      <h3 className={styles.subtitle}>¿Qué querés saber?</h3>
+      <ul className={styles.list}>
+        <li>Costo y tiempo de envío</li>
+        <li>Devoluciones gratis</li>
+        <li>Medios de pago y promociones</li>
+        <li>Garantía</li>
+      </ul>
+      <div className="qa">
+        <label>
+        Preguntale al vendedor
+        <input type="text" />
+        <input type="submit" value="Preguntar" />
+        </label>
+      </div>
+      <div className={styles.container}>
+      <h3>Últimas realizadas</h3>
+      <span>
+        <p style={{color: "black"}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit voluptates sed in perspiciatis placeat quo laborum, culpa, adipisci eius eaque magnam, sunt ullam veniam incidunt quae illo iste iure natus?</p>
+        <p style={{color: "gray"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos ad, numquam atque quis maxime dolores adipisci voluptates. Harum laboriosam quis, libero dolores cupiditate ipsum, omnis ab maxime vel dolor tempore!
+        Facilis distinctio vitae temporibus, dolorum quidem explicabo quos sunt ipsam culpa aliquam repudiandae tempore? Dolorum, eius minima? Consectetur error, similique eveniet sed adipisci, cumque at quibusdam provident officia soluta aut?
+        Non optio deserunt numquam eius sequi voluptas reprehenderit quo ab possimus atque repudiandae illo ipsum, quia, necessitatibus laudantium mollitia accusamus modi vero adipisci architecto earum hic consequatur, nesciunt repellat? Sequi.</p>
+      </span>
+      <span>
+        <p style={{color: "black"}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit voluptates sed in perspiciatis placeat quo laborum, culpa, adipisci eius eaque magnam, sunt ullam veniam incidunt quae illo iste iure natus?</p>
+        <p style={{color: "gray"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos ad, numquam atque quis maxime dolores adipisci voluptates. Harum laboriosam quis, libero dolores cupiditate ipsum, omnis ab maxime vel dolor tempore!
+        Facilis distinctio vitae temporibus, dolorum quidem explicabo quos sunt ipsam culpa aliquam repudiandae tempore? Dolorum, eius minima? Consectetur error, similique eveniet sed adipisci, cumque at quibusdam provident officia soluta aut?
+        Non optio deserunt numquam eius sequi voluptas reprehenderit quo ab possimus atque repudiandae illo ipsum, quia, necessitatibus laudantium mollitia accusamus modi vero adipisci architecto earum hic consequatur, nesciunt repellat? Sequi.</p>
+      </span>
+      <span>
+        <p style={{color: "black"}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit voluptates sed in perspiciatis placeat quo laborum, culpa, adipisci eius eaque magnam, sunt ullam veniam incidunt quae illo iste iure natus?</p>
+        <p style={{color: "gray"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos ad, numquam atque quis maxime dolores adipisci voluptates. Harum laboriosam quis, libero dolores cupiditate ipsum, omnis ab maxime vel dolor tempore!
+        Facilis distinctio vitae temporibus, dolorum quidem explicabo quos sunt ipsam culpa aliquam repudiandae tempore? Dolorum, eius minima? Consectetur error, similique eveniet sed adipisci, cumque at quibusdam provident officia soluta aut?
+        Non optio deserunt numquam eius sequi voluptas reprehenderit quo ab possimus atque repudiandae illo ipsum, quia, necessitatibus laudantium mollitia accusamus modi vero adipisci architecto earum hic consequatur, nesciunt repellat? Sequi.</p>
+      </span>
+      </div>
+    </div>
+  );
 }
 
-export default Questions
+export default Questions;
