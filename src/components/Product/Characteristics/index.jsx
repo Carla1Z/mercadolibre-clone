50958463496cdd61b41750aa7b3f92734e156ac4
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-01-09 16:45:15 -0300

    maquetado caracteristicas

--- src/components/Product/Characteristics/index.jsx
+++ src/components/Product/Characteristics/index.jsx
@@ -1,9 +1,42 @@
-import styles from "./Characteristics.module.css"
+import styles from "./Characteristics.module.css";
 
 function Characteristics() {
   return (
-    <div className={styles.characteristics}>Characteristics</div>
-  )
+    <div className={styles.characteristics}>
+      <h2 className={styles.title}>Características principales</h2>
+      <table>
+        <tr>
+          <th>Fabricante</th>
+          <td>Carla</td>
+        </tr>
+        <tr>
+          <th>Fabricante</th>
+          <td>Carla</td>
+        </tr>
+        <tr>
+          <th>Fabricante</th>
+          <td>Carla</td>
+        </tr>
+        <tr>
+          <th>Fabricante</th>
+          <td>Carla</td>
+        </tr>
+      </table>
+      <h3 className={styles.subtitle}>Otras características</h3>
+      <ul>
+        <li>aaa</li>
+        <li>bbb</li>
+      </ul>
+      <ul>
+        <li>aaa</li>
+        <li>bbb</li>
+      </ul>
+      <ul>
+        <li>aaa</li>
+        <li>bbb</li>
+      </ul>
+    </div>
+  );
 }
 
-export default Characteristics
+export default Characteristics;
