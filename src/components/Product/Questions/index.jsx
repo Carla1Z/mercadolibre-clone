3c0f509d1336e26ec309a24f2297d4779289fb92
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-01-09 01:14:07 -0300

    container questions

--- src/components/Product/Questions/index.jsx
+++ src/components/Product/Questions/index.jsx
@@ -1,4 +1,5 @@
 import styles from "./Questions.module.css";
+import { BsArrowReturnRight } from "react-icons/bs";
 
 function Questions() {
   return (
@@ -16,75 +17,84 @@ function Questions() {
       <div className={styles.qa}>
         <p className={styles.makequestion}>Preguntale al vendedor</p>
         <form className={styles.form}>
-          <input type="text" placeholder="Escribí tu pregunta..." className={styles.text} />
+          <input
+            type="text"
+            placeholder="Escribí tu pregunta..."
+            className={styles.text}
+          />
           <input type="submit" value="Preguntar" className={styles.button} />
         </form>
       </div>
       <div className={styles.container}>
-        <h3>Últimas realizadas</h3>
-        <span>
-          <p style={{ color: "black" }}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit
-            voluptates sed in perspiciatis placeat quo laborum, culpa, adipisci
-            eius eaque magnam, sunt ullam veniam incidunt quae illo iste iure
-            natus?
-          </p>
-          <p style={{ color: "gray" }}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos ad,
-            numquam atque quis maxime dolores adipisci voluptates. Harum
-            laboriosam quis, libero dolores cupiditate ipsum, omnis ab maxime
-            vel dolor tempore! Facilis distinctio vitae temporibus, dolorum
-            quidem explicabo quos sunt ipsam culpa aliquam repudiandae tempore?
-            Dolorum, eius minima? Consectetur error, similique eveniet sed
-            adipisci, cumque at quibusdam provident officia soluta aut? Non
-            optio deserunt numquam eius sequi voluptas reprehenderit quo ab
-            possimus atque repudiandae illo ipsum, quia, necessitatibus
-            laudantium mollitia accusamus modi vero adipisci architecto earum
-            hic consequatur, nesciunt repellat? Sequi.
-          </p>
-        </span>
-        <span>
-          <p style={{ color: "black" }}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit
-            voluptates sed in perspiciatis placeat quo laborum, culpa, adipisci
-            eius eaque magnam, sunt ullam veniam incidunt quae illo iste iure
-            natus?
-          </p>
-          <p style={{ color: "gray" }}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos ad,
-            numquam atque quis maxime dolores adipisci voluptates. Harum
-            laboriosam quis, libero dolores cupiditate ipsum, omnis ab maxime
-            vel dolor tempore! Facilis distinctio vitae temporibus, dolorum
-            quidem explicabo quos sunt ipsam culpa aliquam repudiandae tempore?
-            Dolorum, eius minima? Consectetur error, similique eveniet sed
-            adipisci, cumque at quibusdam provident officia soluta aut? Non
-            optio deserunt numquam eius sequi voluptas reprehenderit quo ab
-            possimus atque repudiandae illo ipsum, quia, necessitatibus
-            laudantium mollitia accusamus modi vero adipisci architecto earum
-            hic consequatur, nesciunt repellat? Sequi.
-          </p>
-        </span>
-        <span>
-          <p style={{ color: "black" }}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit
-            voluptates sed in perspiciatis placeat quo laborum, culpa, adipisci
-            eius eaque magnam, sunt ullam veniam incidunt quae illo iste iure
-            natus?
-          </p>
-          <p style={{ color: "gray" }}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos ad,
-            numquam atque quis maxime dolores adipisci voluptates. Harum
-            laboriosam quis, libero dolores cupiditate ipsum, omnis ab maxime
-            vel dolor tempore! Facilis distinctio vitae temporibus, dolorum
-            quidem explicabo quos sunt ipsam culpa aliquam repudiandae tempore?
-            Dolorum, eius minima? Consectetur error, similique eveniet sed
-            adipisci, cumque at quibusdam provident officia soluta aut? Non
-            optio deserunt numquam eius sequi voluptas reprehenderit quo ab
-            possimus atque repudiandae illo ipsum, quia, necessitatibus
-            laudantium mollitia accusamus modi vero adipisci architecto earum
-            hic consequatur, nesciunt repellat? Sequi.
-          </p>
-        </span>
+        <h3 className={styles.lastquestions}>Últimas realizadas</h3>
+        <div className={styles.containerquestions}>
+          <span>
+            <p className={styles.userquestion}>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit
+              voluptates sed in perspiciatis placeat quo laborum, culpa,
+              adipisci eius eaque magnam, sunt ullam veniam incidunt quae illo
+              iste iure natus?
+            </p>
+            <p className={styles.answer}>
+              <BsArrowReturnRight />
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos ad,
+              numquam atque quis maxime dolores adipisci voluptates. Harum
+              laboriosam quis, libero dolores cupiditate ipsum, omnis ab maxime
+              vel dolor tempore! Facilis distinctio vitae temporibus, dolorum
+              quidem explicabo quos sunt ipsam culpa aliquam repudiandae
+              tempore? Dolorum, eius minima? Consectetur error, similique
+              eveniet sed adipisci, cumque at quibusdam provident officia soluta
+              aut? Non optio deserunt numquam eius sequi voluptas reprehenderit
+              quo ab possimus atque repudiandae illo ipsum, quia, necessitatibus
+              laudantium mollitia accusamus modi vero adipisci architecto earum
+              hic consequatur, nesciunt repellat? Sequi.
+            </p>
+          </span>
+          <span>
+            <p className={styles.userquestion}>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit
+              voluptates sed in perspiciatis placeat quo laborum, culpa,
+              adipisci eius eaque magnam, sunt ullam veniam incidunt quae illo
+              iste iure natus?
+            </p>
+            <p className={styles.answer}>
+              <BsArrowReturnRight />
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos ad,
+              numquam atque quis maxime dolores adipisci voluptates. Harum
+              laboriosam quis, libero dolores cupiditate ipsum, omnis ab maxime
+              vel dolor tempore! Facilis distinctio vitae temporibus, dolorum
+              quidem explicabo quos sunt ipsam culpa aliquam repudiandae
+              tempore? Dolorum, eius minima? Consectetur error, similique
+              eveniet sed adipisci, cumque at quibusdam provident officia soluta
+              aut? Non optio deserunt numquam eius sequi voluptas reprehenderit
+              quo ab possimus atque repudiandae illo ipsum, quia, necessitatibus
+              laudantium mollitia accusamus modi vero adipisci architecto earum
+              hic consequatur, nesciunt repellat? Sequi.
+            </p>
+          </span>
+          <span>
+            <p className={styles.userquestion}>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit
+              voluptates sed in perspiciatis placeat quo laborum, culpa,
+              adipisci eius eaque magnam, sunt ullam veniam incidunt quae illo
+              iste iure natus?
+            </p>
+            <p className={styles.answer}>
+              <BsArrowReturnRight />
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos ad,
+              numquam atque quis maxime dolores adipisci voluptates. Harum
+              laboriosam quis, libero dolores cupiditate ipsum, omnis ab maxime
+              vel dolor tempore! Facilis distinctio vitae temporibus, dolorum
+              quidem explicabo quos sunt ipsam culpa aliquam repudiandae
+              tempore? Dolorum, eius minima? Consectetur error, similique
+              eveniet sed adipisci, cumque at quibusdam provident officia soluta
+              aut? Non optio deserunt numquam eius sequi voluptas reprehenderit
+              quo ab possimus atque repudiandae illo ipsum, quia, necessitatibus
+              laudantium mollitia accusamus modi vero adipisci architecto earum
+              hic consequatur, nesciunt repellat? Sequi.
+            </p>
+          </span>
+        </div>
       </div>
     </div>
   );
