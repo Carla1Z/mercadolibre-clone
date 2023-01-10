3dde9173abffc781deee2483a21b80a95ff8226c
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-01-10 16:36:45 -0300

    header collumn two

--- src/components/Product/Buy/index.jsx
+++ src/components/Product/Buy/index.jsx
@@ -1,20 +1,49 @@
-import styles from "./Buy.module.css"
+import { AiFillHeart, AiFillStar, AiOutlineHeart } from "react-icons/ai";
+import ReactStars from "react-stars";
+import styles from "./Buy.module.css";
 
 function Buy() {
   return (
     <div className={styles.buy}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, asperiores a necessitatibus pariatur recusandae reprehenderit corporis vel et nemo dicta, assumenda explicabo voluptatibus praesentium in possimus, eveniet quasi nostrum magni?
-      Voluptates temporibus velit, assumenda, nisi, at cumque atque molestiae iste nemo natus impedit. Nulla et quisquam vel culpa ipsam magni! Natus minima aperiam accusantium magnam ea atque harum, et fugit?
-      Nulla ratione iusto quos laborum id voluptate sed voluptates quod, sequi cum aspernatur! Temporibus rerum alias fugit. Libero quaerat harum, reprehenderit deleniti accusantium doloribus porro! Eos ratione fugit perspiciatis excepturi?
-      Sed deleniti laudantium magni. Atque libero mollitia laudantium! Maiores qui eaque error pariatur tempore perferendis, amet optio. Eius, sed laboriosam dolorem, modi architecto doloremque temporibus itaque provident molestiae, sint at?
-      Consequuntur eaque minima, eum inventore labore suscipit quidem magnam dolor odit ab adipisci soluta nisi, sapiente iure aspernatur in error a officia cupiditate aut cum nostrum! Consequatur, dolores itaque! Optio!
-      Perspiciatis aut, voluptatibus ab architecto iure est repellat nemo rerum laboriosam dolore ipsum amet molestiae eligendi doloremque? Pariatur distinctio rem voluptatum officia quam ducimus, ab praesentium similique? Nemo, sunt provident.
-      Rerum illum non sed deleniti provident voluptatum, molestiae porro nobis aliquid, cupiditate quas sunt voluptatem sapiente facere quaerat incidunt! Unde molestias illum sunt laudantium minima ratione ipsum minus recusandae nihil.
-      Ipsam, minima delectus cupiditate, sunt tenetur sint laborum et quam blanditiis culpa ipsum voluptatem quasi doloremque temporibus esse provident iure mollitia fugit modi deleniti! Dolor id qui perferendis sapiente soluta!
-      Officiis necessitatibus adipisci rem reprehenderit omnis libero obcaecati, quia sit odio aspernatur exercitationem ducimus ullam pariatur hic accusantium quam sint molestias fugiat eos provident repudiandae? Ea vero ipsam porro totam.
-      Quaerat quasi voluptatibus voluptas quis, autem numquam perspiciatis maiores, beatae amet culpa possimus ut laudantium minima quod repellat enim magni aliquid, est neque nisi? Nisi reprehenderit tempora nam ipsa adipisci?
+      <div className={styles.header}>
+        <p className={styles.headerInfo}>Nuevo | 920 vendidos</p>
+        <div className={styles.title}>
+          <h1>Anillo Plata 925 Y Oro Con Piedra Muy Elegantes Delicados</h1>
+          <button>
+            <AiOutlineHeart />
+          </button>
+          {/* <button><AiFillHeart /></button> */}
+        </div>
+        <span className={styles.rating}>
+          <ReactStars
+            count={5}
+            value={3}
+            size={20}
+            char={<AiFillStar />}
+            edit={false}
+            color2={"#3483fa"}
+          />
+          <p>(63)</p>
+        </span>
+      </div>
+      <div className={styles.highlights}>
+        <p>MÁS VENDIDO</p>
+        <p>19° en Anillos</p>
+      </div>
+      <div className={styles.price}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className={styles.shippingsummary}></div>
+      <div className={styles.returnssummary}></div>
+      <div className={styles.variations}></div>
+      <div className={styles.stockinformation}></div>
+      <div className={styles.availablequantity}></div>
+      <div className={styles.mainactions}></div>
+      <div className={styles.buybenefits}></div>
     </div>
-  )
+  );
 }
 
-export default Buy
+export default Buy;
